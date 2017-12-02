7f48b9336aaeeca2c54c2fa728321459b0fa4e16
didier <dgautheron@magic.fr>
2017-12-02 21:51:34 +0100

    backport GribRecord stuff from OpenCPN

--- src/GribRecordSet.h
+++ src/GribRecordSet.h
@@ -39,7 +39,7 @@ enum { Idx_WIND_VX, Idx_WIND_VX850, Idx_WIND_VX700, Idx_WIND_VX500, Idx_WIND_VX3
 
 class GribRecordSet {
 public:
-    GribRecordSet() {
+    GribRecordSet(unsigned int id) : m_Reference_Time(-1), m_ID(id) {
         for(int i=0; i<Idx_COUNT; i++) {
             m_GribRecordPtrArray[i] = 0;
             m_GribRecordCopied[i] = false;
@@ -61,6 +61,8 @@ public:
     }
 
     time_t m_Reference_Time;
+    unsigned int m_ID;
+
     GribRecord *m_GribRecordPtrArray[Idx_COUNT];
 private:
     bool        m_GribRecordCopied[Idx_COUNT];
