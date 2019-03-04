e13d49393f5d7aacfce647fc6395071a7a3d2016
didier <didier@users.sourceforge.net>
2019-03-04 21:47:46 +0100

    mingw test for system tinyxml library

--- cmake/PluginXML.cmake
+++ cmake/PluginXML.cmake
@@ -6,13 +6,24 @@
 
 
 IF(WIN32)
-SET(SRC_TINYXML
+    IF(MINGW)
+        FIND_PACKAGE(TinyXML)
+    ENDIF()
+
+    IF(TINYXML_FOUND)
+        message (STATUS "Building with system tinyxml")
+        INCLUDE_DIRECTORIES(${TINYXML_INCLUDE_DIR})
+        TARGET_LINK_LIBRARIES(${PACKAGE_NAME} ${TINYXML_LIBRARIES} )
+    ELSE()
+        message (STATUS "Building with bundled tinyxml")
+        SET(SRC_TINYXML
             src/tinyxml/tinyxml.cpp
             src/tinyxml/tinyxmlparser.cpp
             src/tinyxml/tinyxmlerror.cpp
-)
-ADD_LIBRARY(${PACKAGE_NAME}_LIB_PLUGINXML STATIC ${SRC_TINYXML})
-TARGET_LINK_LIBRARIES( ${PACKAGE_NAME} ${PACKAGE_NAME}_LIB_PLUGINXML )
-ENDIF(WIN32)
+        )
+        ADD_LIBRARY(${PACKAGE_NAME}_LIB_PLUGINXML STATIC ${SRC_TINYXML})
+        TARGET_LINK_LIBRARIES( ${PACKAGE_NAME} ${PACKAGE_NAME}_LIB_PLUGINXML )
+    ENDIF()
+ENDIF()
 
 ADD_DEFINITIONS(-DTIXML_USE_STL)
